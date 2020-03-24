684221101ed0ae64579a9d70e48109b5287a4773
Muhammad Syulhan <janmuno77@gmail.com>
2020-03-24 23:17:55 +0800

    tambahin list buat isi view students

--- cobaLaravel/resources/views/students/index.blade.php
+++ cobaLaravel/resources/views/students/index.blade.php
@@ -6,9 +6,18 @@
 <div class="container">
     <div class="row">
         <!-- biar dia agak ke tengah -->
-        <div class="col-10">
+        <!-- kecilin nomornya biar lbh pendek kolomnya -->
+        <div class="col-6">
         <!-- class mt-2 biar agak turun ke bawah -->
             <h1 class="mt-2">Daftar Students!</h1>
+                <ul class="list-group">
+                    @foreach( $students as $students)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            {{ $students->nama }}
+                            <a href="" class="badge badge-info">detail</a>
+                        </li>
+                    @endforeach
+                </ul>
         </div>
     </div>
 </div>
